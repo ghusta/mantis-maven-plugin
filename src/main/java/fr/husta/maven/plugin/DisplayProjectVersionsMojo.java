ccf52f34f6ad882c4fd2de93798136ed9b54285f
Guillaume Husta <guillaume.husta@gmail.com>
2012-11-07 00:18:46 +0100

    Issue #7

---
+++ src/main/java/fr/husta/maven/plugin/DisplayProjectVersionsMojo.java
@@ -0,0 +1,107 @@
+package fr.husta.maven.plugin;
+
+import java.math.BigInteger;
+import java.rmi.RemoteException;
+
+import javax.xml.rpc.ServiceException;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import biz.futureware.mantis.rpc.soap.client.ProjectVersionData;
+import fr.husta.maven.plugin.util.MantisConnector;
+import fr.husta.maven.plugin.util.MantisUtils;
+
+/**
+ * Displays the versions of a Mantis project.
+ * 
+ * @goal display-project-versions
+ * @requiresProject true
+ * 
+ */
+public class DisplayProjectVersionsMojo extends AbstractSecureMantisMojo {
+
+	/**
+	 * @parameter default-value="${project}"
+	 * @required
+	 * @readonly
+	 */
+	protected MavenProject project;
+
+	/**
+	 * @parameter expression="${mantis.projectName}"
+	 *            default-value="${project.artifactId}"
+	 * @required
+	 */
+	protected String projectName;
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		try {
+			// connection to Mantis SOAP API
+			MantisConnectPortType portType = MantisUtils
+					.createConnector(getMantisSoapApiUrl());
+			MantisConnector mantisConnector = new MantisConnector(portType);
+
+			getLog().debug("projectName = '" + projectName + "'");
+
+			// find ProjectId from Name
+			BigInteger projectId = mantisConnector.getProjectIdByName(login,
+					password, projectName);
+			// call to web service method
+			ProjectVersionData[] projectVersions = mantisConnector
+					.getProjectVersions(login, password, projectId);
+			// TODO: sort versions
+			
+			// display to log
+			getLog().info(
+					"Project : " + projectName + " (id = " + projectId + ")");
+			displayVersionList(projectVersions);
+
+		} catch (ServiceException e) {
+			// getLog().error(e.getMessage());
+			throw new MojoExecutionException(e.getMessage(), e);
+		} catch (RemoteException e) {
+			// getLog().error(e.getMessage());
+			throw new MojoExecutionException(e.getMessage(), e);
+		}
+
+	}
+
+	/**
+	 * Display versions to log.
+	 * 
+	 * @param projectVersions
+	 */
+	private void displayVersionList(ProjectVersionData[] projectVersions) {
+		getLog().info("Versions : ");
+		// header
+		getLog().info(StringUtils.repeat("*", 60));
+		getLog().info(
+				"*" + StringUtils.center("NAME", 30) + "*"
+						+ StringUtils.center("RELEASED", 20) + "*"
+						+ StringUtils.center("OBSOLETE", 20) + "*");
+		for (int i = 0; i < projectVersions.length; i++) {
+			displayVersion(projectVersions[i]);
+		}
+		// footer
+		getLog().info(StringUtils.repeat("*", 60));
+	}
+
+	private void displayVersion(ProjectVersionData projectVersionData) {
+		getLog().info(
+				"*"
+						+ StringUtils.left(projectVersionData.getName(), 30)
+						+ "*"
+						+ StringUtils.center(
+								projectVersionData.getReleased() ? "X" : " ",
+								20)
+						+ "*"
+						+ StringUtils.center(
+								projectVersionData.getObsolete() ? "X" : " ",
+								20) + "*");
+	}
+}
--- src/main/java/fr/husta/maven/plugin/util/MantisConnector.java
+++ src/main/java/fr/husta/maven/plugin/util/MantisConnector.java
@@ -10,79 +10,90 @@ import biz.futureware.mantis.rpc.soap.client.ProjectVersionData;
  * Delegate to MantisConnectPortType.
  * 
  * @author Guillaume
- *
+ * 
  */
-public class MantisConnector
-{
+public class MantisConnector {
 
-    private MantisConnectPortType mantisConnectPortType;
+	private MantisConnectPortType mantisConnectPortType;
 
-    public MantisConnector(MantisConnectPortType portType)
-    {
-        this.mantisConnectPortType = portType;
-    }
+	public MantisConnector(MantisConnectPortType portType) {
+		this.mantisConnectPortType = portType;
+	}
 
-    /**
-     * 
-     * @return
-     * @throws RemoteException
-     */
-    public String getMantisVersion() throws RemoteException
-    {
-        String version = this.mantisConnectPortType.mc_version();
+	/**
+	 * 
+	 * @return
+	 * @throws RemoteException
+	 */
+	public String getMantisVersion() throws RemoteException {
+		String version = this.mantisConnectPortType.mc_version();
 
-        return version;
-    }
+		return version;
+	}
 
-    /**
-     * 
-     * @param username
-     * @param password
-     * @param projectName
-     * @return ProjectId
-     * @throws RemoteException
-     */
-    public BigInteger getProjectIdByName(String username, String password, String projectName)
-            throws RemoteException
-    {
-        BigInteger id = this.mantisConnectPortType.mc_project_get_id_from_name(username, password,
-                projectName);
+	/**
+	 * 
+	 * @param username
+	 * @param password
+	 * @param projectName
+	 * @return ProjectId
+	 * @throws RemoteException
+	 */
+	public BigInteger getProjectIdByName(String username, String password,
+			String projectName) throws RemoteException {
+		BigInteger id = this.mantisConnectPortType.mc_project_get_id_from_name(
+				username, password, projectName);
 
-        // warning returns 0 if not exist !
-        if (BigInteger.ZERO.compareTo(id) == 0)
-        {
-            throw new RuntimeException("Project '" + projectName + "' unknown");
-        }
+		// warning returns 0 if not exist !
+		if (BigInteger.ZERO.compareTo(id) == 0) {
+			throw new RuntimeException("Project '" + projectName + "' unknown");
+		}
 
-        return id;
-    }
+		return id;
+	}
 
-    /**
-     * Simple version of version creation.
-     * Fault if project doesn't exist or version already exists.
-     * 
-     * @param username
-     * @param password
-     * @param projectId
-     * @param versionName
-     * @throws RemoteException
-     */
-    public void addProjectVersion(String username, String password, BigInteger projectId, String versionName)
-            throws RemoteException
-    {
-        ProjectVersionData versionData = new ProjectVersionData();
-        versionData.setProject_id(projectId);
-        versionData.setName(versionName);
-        versionData.setReleased(Boolean.TRUE);
-        versionData.setObsolete(Boolean.FALSE);
+	/**
+	 * Simple version of version creation. Fault if project doesn't exist or
+	 * version already exists.
+	 * 
+	 * @param username
+	 * @param password
+	 * @param projectId
+	 * @param versionName
+	 * @throws RemoteException
+	 */
+	public void addProjectVersion(String username, String password,
+			BigInteger projectId, String versionName) throws RemoteException {
+		ProjectVersionData versionData = new ProjectVersionData();
+		versionData.setProject_id(projectId);
+		versionData.setName(versionName);
+		versionData.setReleased(Boolean.TRUE);
+		versionData.setObsolete(Boolean.FALSE);
 
-        // bug Mantis ?
-        if (versionData.getDescription() == null)
-        {
-            versionData.setDescription("");
-        }
+		// bug Mantis ?
+		if (versionData.getDescription() == null) {
+			versionData.setDescription("");
+		}
 
-        this.mantisConnectPortType.mc_project_version_add(username, password, versionData);
-    }
+		this.mantisConnectPortType.mc_project_version_add(username, password,
+				versionData);
+	}
+
+	/**
+	 * Return a list of ProjectVersionData for the project.
+	 * 
+	 * @param username
+	 * @param password
+	 * @param projectId
+	 * @return
+	 * @throws RemoteException
+	 */
+	public ProjectVersionData[] getProjectVersions(String username,
+			String password, BigInteger projectId) throws RemoteException {
+		ProjectVersionData[] projectVersionDatas = this.mantisConnectPortType
+				.mc_project_get_versions(username, password, projectId);
+
+		return projectVersionDatas;
+	}
 
 }
